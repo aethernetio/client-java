533eac9011e1d77afe4f1b5844c1f255cf19c3dc
MakProg <makshanov@aethernet.io>
2025-04-27 11:00:36 +0800

    ts-client

--- chat/src/test/java/io/aether/examples/plainChat/RegistrationTest.java
+++ chat/src/test/java/io/aether/examples/plainChat/RegistrationTest.java
@@ -3,6 +3,7 @@ package io.aether.examples.plainChat;
 import io.aether.StandardUUIDs;
 import io.aether.cloud.client.AetherCloudClient;
 import io.aether.cloud.client.ClientStateInMemory;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.net.URI;
@@ -10,14 +11,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RegistrationTest {
-    public ClientStateInMemory clientConfig1;
     public final List<URI> cloudFactoryURI = new ArrayList<>();
+    public ClientStateInMemory clientConfig1;
 
     @Test
     public void main() {
         if (clientConfig1 == null) clientConfig1 = new ClientStateInMemory(StandardUUIDs.TEST_UID, cloudFactoryURI);
         AetherCloudClient client1 = new AetherCloudClient(clientConfig1);
         client1.startFuture.waitDoneSeconds(10);
-        client1.stop(5);
+        Assertions.assertTrue(client1.destroy(true).waitDoneSeconds(5));
     }
 }
