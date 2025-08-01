a68789ecae93a58e598c8cae407145bcf6b18a4a
MakProg <makshanov@aethernet.io>
2025-08-02 03:04:14 +0800

    gradle plugin

--- cli/src/main/java/io/aether/cli/CLI.java
+++ cli/src/main/java/io/aether/cli/CLI.java
@@ -5,7 +5,7 @@ import io.aether.common.ServerDescriptor;
 import io.aether.crypt.CryptoLib;
 import io.aether.logger.Log;
 import io.aether.utils.AString;
-import io.aether.utils.CType;
+import io.aether.utils.CTypeI;
 import io.aether.utils.RU;
 import io.aether.utils.consoleCanonical.ConsoleMgrCanonical;
 import io.aether.utils.flow.Flow;
@@ -27,9 +27,9 @@ public class CLI {
 
         };
         c.footer = "For more information, please visit the website https://aethernet.io";
-        c.regConverter(CType.of(CryptoLib.class), CryptoLib::valueOf);
-        c.regResultConverter("bin", CType.of(ClientStateInMemory.class), ClientStateInMemory::save);
-        c.regResultConverterCtx("bin", CType.of(CliApi.Msg.class), (ctx, v) -> {
+        c.regConverter(CTypeI.of(CryptoLib.class), CryptoLib::valueOf);
+        c.regResultConverter("bin", CTypeI.of(ClientStateInMemory.class), ClientStateInMemory::save);
+        c.regResultConverterCtx("bin", CTypeI.of(CliApi.Msg.class), (ctx, v) -> {
             if (ctx.isToFile()) {
                 if (ctx.getFileName() == null) {
                     ctx.setFileName(v.address.toString());
@@ -45,7 +45,7 @@ public class CLI {
             }
             return v.data;
         });
-        c.regResultConverterCtx("json", CType.of(CliApi.Msg.class), (ctx, v) -> {
+        c.regResultConverterCtx("json", CTypeI.of(CliApi.Msg.class), (ctx, v) -> {
             if (ctx.isToFile()) {
                 if (ctx.getFileName() == null) {
                     ctx.setFileName(v.address.toString());
@@ -65,7 +65,7 @@ public class CLI {
             );
             return RU.toJson(m).toString().getBytes(StandardCharsets.UTF_8);
         });
-        c.regResultConverterCtx("hex", CType.of(CliApi.Msg.class), (ctx, v) -> {
+        c.regResultConverterCtx("hex", CTypeI.of(CliApi.Msg.class), (ctx, v) -> {
             if (ctx.isToFile()) {
                 if (ctx.getFileName() == null) {
                     ctx.setFileName(v.address.toString());
@@ -81,7 +81,7 @@ public class CLI {
             }
             return RU.toHexString(v.data).getBytes();
         });
-        c.regResultConverterCtx("utf8", CType.of(CliApi.Msg.class), (ctx, v) -> {
+        c.regResultConverterCtx("utf8", CTypeI.of(CliApi.Msg.class), (ctx, v) -> {
             if (ctx.isToFile()) {
                 var s = AString.of();
                 s.add(v.address).add(" -> ").add(new String(v.data));
@@ -92,7 +92,7 @@ public class CLI {
                 return s.getBytes();
             }
         });
-        c.regResultConverter("json", CType.of(ClientStateInMemory.class), v -> {
+        c.regResultConverter("json", CTypeI.of(ClientStateInMemory.class), v -> {
             Map<String, Object> m = Map.of(
                     "uid", v.getUid(),
                     "alias", v.getAlias(),
